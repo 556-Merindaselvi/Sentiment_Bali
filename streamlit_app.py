b7be976439465d8a4ed27bdf6ba5cfc65b321ef1
merin <91817079+556-Merindaselvi@users.noreply.github.com>
2026-06-01 13:09:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+st.set_page_config(
+    page_title="Analisis Sentimen Sampah Plastik Bali",
+    layout="wide"
+)
+
+st.title("📊 Dashboard Analisis Sentimen Pengelolaan Sampah Plastik di Bali")
+
+menu = st.sidebar.selectbox(
+    "Menu",
+    [
+        "Overview",
+        "Distribusi Sentimen",
+        "Perbandingan Model",
+        "Confusion Matrix",
+        "Insight Penelitian"
+    ]
+)
